a1e75b3def0e068c00f8a9066d8bdbbcf2c80cd6
dswallach <dawallach@vassar.edu>
2017-04-05 13:27:50 -0400

    Added tests for openTrial

--- src/reducers/openTrialReducers.jsx
+++ src/reducers/openTrialReducers.jsx
@@ -1,23 +1,13 @@
 const openTrial = (state = -1, action) => {
-    console.log('OpenTrialReducer');
     switch (action.type) {
     case 'INITIAL_STATE':
+    case 'CLOSE_DRAWER':
         newState = -1;
         return newState;
     case 'SET_STATE':
-        var newState = Object.assign({}, action.state.openTrial);
+        var newState = action.state.openTrial;
         return newState;
     case 'OPEN_DRAWER':
-        var newState = Object.assign({}, state);
-        delete newState['openTrial'];
-        newState['openTrial'] = action.id;
-        return newState;
-    case 'CLOSE_DRAWER':
-        // Create the new state
-        var newState = Object.assign({}, state);
-        delete newState['openTrial'];
-        newState['openTrial'] = -1;
-        return newState;
     case 'SET_OPEN_TRIAL':
         var newState = action.id;
         return newState;
