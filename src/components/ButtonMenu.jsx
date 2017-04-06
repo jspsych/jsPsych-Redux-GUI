16a87d7fa1d1e954a36a617f0592fda2373f8bd8
dswallach <dawallach@vassar.edu>
2017-04-06 09:17:19 -0400

    Fixed new tab opening when hovering over ButtonMenu

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { render, Component, PropTypes } from 'react-dom';
-import Avatar from 'material-ui/Avatar';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
 import Undo from 'material-ui/svg-icons/content/undo';
 import Redo from 'material-ui/svg-icons/content/redo';
 import ContentRemove from 'material-ui/svg-icons/content/remove';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import MenuItem from 'material-ui/MenuItem';
 import IconMenu from 'material-ui/IconMenu';
 import Divider from 'material-ui/Divider';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {actionAddTrial, actionRemoveTrial, actionRestoreState, actionRestoreFutureState } from 'actions';
 
@@ -49,46 +44,63 @@ const removeStyleFAB = {
 
 class ButtonMenu extends React.Component {
 
-    add () { actionAddTrial(this.props.store); }
-    remove () { actionRemoveTrial(this.props.store); }
-    fastForward () { actionRestoreFutureState(this.props.store); }
-    restore () { actionRestoreState(this.props.store); }
+    /* eslint-disable */
+    static propTypes = {
+        store: React.PropTypes.object.isRequired,
+    }
+    /* eslint-enable */
+
+    add (e) {
+        e.preventDefault();
+        actionAddTrial(this.props.store); 
+    }
+    remove (e) {
+        e.preventDefault();
+        actionRemoveTrial(this.props.store);
+    }
+    fastForward (e) {
+        e.preventDefault();
+        actionRestoreFutureState(this.props.store);
+    }
+    restore (e) {
+        e.preventDefault();
+        actionRestoreState(this.props.store); 
+    }
     render () {
         return (
             <IconMenu
-            iconButtonElement={<IconButton> <MoreVertIcon /></IconButton>}
-            style={addStyleFAB}
-            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            onMouseOver={open}
+                iconButtonElement={<IconButton> <MoreVertIcon /></IconButton>}
+                style={addStyleFAB}
+                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                targetOrigin={{horizontal: 'left', vertical: 'bottom'}}
             >
 
             <MenuItem
-            primaryText="Add (ctrl+a)"
-            rightIcon={<ContentAdd />}
-            onTouchTap={this.add.bind(this)}
+                primaryText="Add (ctrl+a)"
+                rightIcon={<ContentAdd />}
+                onTouchTap={this.add.bind(this)}
             />
 
-            <Divider />
-            <MenuItem
+        <Divider />
+        <MenuItem
             primaryText="Remove (ctrl+x / Delete)"
             rightIcon={<ContentRemove />}
             onTouchTap={this.remove.bind(this)}
-            />
+        />
 
-            <Divider />
-            <MenuItem
-            primaryText="Undo (ctrl+z)"
-            rightIcon={<Undo />}
-            onTouchTap={this.restore.bind(this)}
-            />
+    <Divider />
+    <MenuItem
+        primaryText="Undo (ctrl+z)"
+        rightIcon={<Undo />}
+        onTouchTap={this.restore.bind(this)}
+    />
 
-            <Divider />
-            <MenuItem
-            primaryText="Redo (ctrl+q)"
-            rightIcon={<Redo />}
-            onTouchTap={this.fastForward.bind(this)}
-            />
+<Divider />
+<MenuItem
+    primaryText="Redo (ctrl+q)"
+    rightIcon={<Redo />}
+    onTouchTap={this.fastForward.bind(this)}
+/>
 
             </IconMenu>
         );
