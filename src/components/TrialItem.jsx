4e7db9f642bec943fe1349880d0c2b1ed96a7d81
dswallach <dawallach@vassar.edu>
2017-05-26 13:36:43 -0400

    Working on collapsable children

--- src/components/TrialItem.jsx
+++ src/components/TrialItem.jsx
@@ -181,7 +181,7 @@ class TrialItem extends React.Component {
             }
             nestedItems={
                 // If this is a timeline
-                this.props.state.trialTable[this.props.trial].isTimeline ?
+                (this.props.state.trialTable[this.props.trial].isTimeline == 1) ?
                 // Display the nested items
                 this.props.state.trialTable[this.props.trial].timeline.map(child => {
                     var childIden = this.props.state.trialOrder.indexOf(child);
