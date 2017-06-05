a08b18da7c16e6604113d5b16e5cc88a73d9ba6d
GavinQ1 <juqi@vassar.edu>
2017-06-05 16:52:24 -0400

    TimelineNode Reducer; New packages

---
+++ src/common/constants/ActionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_TIMELINE = "ADD_TIMELINE";
+export const DELETE_TIMELINE = "DELETE_TIMELINE";
+export const MOVE_TIMELINE = "MOVE_TIMELINE";
+export const ADD_TRIAL = "ADD_TRIAL";
+export const DELETE_TRIAL = "DELETE_TRIAL";
+export const MOVE_TRIAL = "MOVE_TRIAL";
