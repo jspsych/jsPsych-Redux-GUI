b7158744cc30f7c82c114d1c67c9a14e4c494d22
GavinQ1 <juqi@vassar.edu>
2017-06-16 12:31:53 -0400

    Set up webpack; dev-env

---
+++ src/client/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore , applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import rootReducer from '../common/reducers';
+import App from '../common/containers/App';
+
+const preloadedState = window.__PRELOADED_STATE__;
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+injectTapEventPlugin();
+ReactDOM.render(
+  <Provider store={store}>
+  	<App />
+  </Provider>,
+  document.getElementById('container')
+);
