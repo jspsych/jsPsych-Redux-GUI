2e8445466a46a7b548397e8f4a018325ce7e12e7
GavinQ1 <juqi@vassar.edu>
2017-06-20 18:14:59 -0400

    Relocate function stringify

--- src/common/reducers/TimelineNode/preview.js
+++ src/common/reducers/TimelineNode/preview.js
@@ -1,6 +1,5 @@
 // var babel = require("babel-core");
 
-import { stringify } from '../../utils';
 import { initState as jsPsychInitState } from './jsPsychInit';
 
 const welcomeObj = {
@@ -61,4 +60,46 @@ export function setLiveEditting(state, action) {
 	return Object.assign({}, state, {
 		liveEditting: action.flag,
 	});
+}
+
+/*
+Specially written for stringify obj in this app to generate code
+For functions, turn it to
+{
+	isFunc: true,
+	code: function
+}
+
+*/
+export function stringify(obj) {
+	let type = typeof obj;
+	switch(type) {
+		case 'object':
+			let res = [];
+			if (Array.isArray(obj)) {
+				res.push("[");
+				let i = obj.length;
+				for (let item of obj) {
+					res.push(stringify(item));
+					if (obj-- > 1) {
+						res.push(",");
+					}
+				}
+				res.push("]");
+			} else if (obj.isFunc) { 
+				let code = obj.code.replace(/\n/g, '').replace(/\t/g, '    ');
+				// console.log(babel.transform(code).code);
+				return code;
+			}else {
+				res.push("{");
+				let keys = Object.keys(obj);
+				for (let key of keys) {
+					res.push('"' + key + '"' + ":" + stringify(obj[key]) + ",");  
+				}
+				res.push("}");
+			}
+			return res.join("");
+		default:
+			return JSON.stringify(obj);
+	}
 }
