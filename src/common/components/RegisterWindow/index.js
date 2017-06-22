edf026c7d047c6723c291475cfd5ecd8bd449d5b
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-06-21 22:04:08 -0400

    add basic form fields to registration component

--- src/common/components/RegisterWindow/index.js
+++ src/common/components/RegisterWindow/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class UserMenu extends React.Component {
   constructor(props) {
@@ -10,14 +13,36 @@ export default class UserMenu extends React.Component {
     this.props.hideRegisterWindow();
   };
 
+  handleCreateAccount = () => {
+
+  }
+
   render(){
+    const actions = [
+      <FlatButton
+        label="Not right now"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
     return(
       <div>
         <Dialog
-        title="Create Account"
-        open={this.props.open}
-        onRequestClose={this.handleClose}>
+          title="Create a new account"
+          open={this.props.open}
+          onRequestClose={this.handleClose}
+          actions={actions}
+          contentStyle={{width: '320px'}}
+        >
         This box is where the account creation form will go.
+          <TextField id="userName" floatingLabelText="Username"></TextField>
+          <TextField id="email" floatingLabelText="Email"></TextField>
+          <TextField id="password" type="password" floatingLabelText="Password"></TextField>
+          <div style={{margin:'auto', textAlign: 'center'}}>
+            <RaisedButton label="Create Account" primary={true} onTouchTap={this.handleCreateAccount} />
+          </div>
         </Dialog>
       </div>
     )
