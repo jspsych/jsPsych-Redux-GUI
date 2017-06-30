ce1fe030f47c6263a4cd2409fe3188e08327feaa
Josh de Leeuw <josh.deleeuw@gmail.com>
2017-06-30 17:17:53 -0400

    most of s3 functionality in place
    still need to partition to folders in bucket and set access policy

--- src/common/backend/s3/index.js
+++ src/common/backend/s3/index.js
@@ -1,7 +1,7 @@
 import AWS from '../aws';
 const bucketName = "jspsych-builder";
 
-function uploadFile(file){
+function uploadFile(file, success, failure){
 
   var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
@@ -14,15 +14,73 @@ function uploadFile(file){
     }, function(err, data) {
       if (err) {
         console.log(err);
+        failure();
         return;
       }
-      alert('Successfully uploaded file.');
+      success();
     });
 
 }
 
-export function upload(files){
+function deleteFile(file, success, failure){
+
+  var s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {Bucket: bucketName}
+  });
+
+  s3.deleteObject({
+      Key: file
+    }, function(err, data) {
+      if (err) {
+        console.log(err);
+        failure();
+        return;
+      }
+      success();
+    });
+
+}
+
+export function listBucketContents(success, failure){
+
+  var s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {Bucket: bucketName}
+  });
+
+  s3.listObjectsV2({}, function(err, data){
+    if(err) {
+      console.log(err);
+      failure(err);
+    } else {
+      //console.log(data);
+      success(data);
+    }
+  });
+
+}
+
+export function upload(files, success, failure){
+  var completed = 0;
   for(var i=0; i<files.length; i++){
-    uploadFile(files[i]);
+    uploadFile(files[i], function(){
+      completed++;
+      if(completed == files.length){
+        success();
+      }
+    }, failure);
+  }
+}
+
+export function deleteObjects(files, success, failure){
+  var completed = 0;
+  for(var i=0; i<files.length; i++){
+    deleteFile(files[i], function(){
+      completed++;
+      if(completed == files.length){
+        success();
+      }
+    }, failure);
   }
 }
--- src/common/components/Appbar/MediaManager/index.js
+++ src/common/components/Appbar/MediaManager/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
+import Checkbox from 'material-ui/Checkbox';
 import Dropzone from 'react-dropzone';
-import { upload } from '../../../backend/s3';
+import CircularProgress from 'material-ui/CircularProgress';
+const mime = require('mime-types');
+import {List, ListItem} from 'material-ui/List';
+import { upload, deleteObjects, listBucketContents } from '../../../backend/s3';
 // import TextField from 'material-ui/TextField';
 
 import {
@@ -12,41 +16,127 @@ import {
   green500 as checkColor,
 } from 'material-ui/styles/colors';
 import MediaManagerIcon from 'material-ui/svg-icons/image/photo-library';
+import ImageIcon from 'material-ui/svg-icons/image/photo';
+import MovieIcon from 'material-ui/svg-icons/image/movie-creation';
+import AudioIcon from 'material-ui/svg-icons/image/audiotrack';
+import FileIcon from 'material-ui/svg-icons/editor/insert-drive-file';
+import PDFIcon from 'material-ui/svg-icons/image/picture-as-pdf';
+import CheckNoIcon from 'material-ui/svg-icons/toggle/check-box-outline-blank';
+import CheckYesIcon from 'material-ui/svg-icons/toggle/check-box';
 
-export default class jsPsychInitEditor extends React.Component {
+function fileIconFromTitle(title) {
+	const type = mime.lookup(title);
+	if(type == false){
+		return <FileIcon />
+	} else if(type.indexOf('image') > -1){
+		return <ImageIcon />
+	} else if(type.indexOf('pdf') > -1){
+		return <PDFIcon />
+	} else if(type.indexOf('video') > -1){
+		return <MovieIcon />
+	} else if(type.indexOf('audio') > -1){
+		return <AudioIcon />
+	}
+	return <FileIcon />
+}
+
+export default class MediaManager extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       open: false,
 			files: [],
+			s3files: {},
+			dropzoneActive: false,
+			uploadComplete: true,
+			selected: []
     };
 
+		this.handleEnter = () => {
+			this.setState({dropzoneActive: true});
+		}
+
+		this.handleExit = () => {
+			console.log('exit dropzone');
+			this.setState({dropzoneActive: false});
+		}
+
     this.handleOpen = () => {
+			this.updateList();
       this.setState({
-        open: true
+        open: true,
+				dropzoneActive: false,
       });
     };
 
     this.handleClose = () => {
       this.setState({
-        open: false
+        open: false,
+				dropzoneActive: false,
       });
     };
 
 		this.onDrop = (files) => {
-			this.setState({files: files})
+			this.setState({dropzoneActive: false});
+			this.handleUpload(files)
 		}
 
-		this.handleUpload = () => {
-			console.log(this.state.files);
+		this.handleSelect = (i) => {
+			let selected = this.state.selected;
+			selected[i] = !selected[i];
+			this.setState({selected: selected});
+		}
+
+		this.handleUpload = (files) => {
 			//console.log(s3);
-			upload(this.state.files);
+			this.setState({uploadComplete: false});
+			upload(files,
+			() => {
+				this.setState({uploadComplete: true});
+				this.updateList();
+			});
+		}
+
+		this.handleDelete = () => {
+			var keys = [];
+			for(var i=0;i<this.state.selected.length;i++){
+				if(this.state.selected[i]){
+					keys.push(this.state.s3files.Contents[i].Key)
+				}
+			}
+			deleteObjects(keys,
+				() => {
+					this.updateList();
+				},
+				() => {}
+			)
+		}
+
+		this.updateList = () => {
+			listBucketContents(
+				(data) => {
+					let selected = [];
+					for(var i=0; i<data.Contents.length; i++){
+						selected.push(false);
+					}
+					this.setState({
+						s3files: data,
+						selected: selected,
+					});
+					console.log('files updated!')
+				},
+				(err) => {},
+			)
 		}
   }
 
   render() {
     const actions = [
+			<FlatButton
+				label="Delete Selected Items"
+				onTouchTap={this.handleDelete}
+			/>,
       <FlatButton
         label="Close"
         primary={true}
@@ -54,6 +144,33 @@ export default class jsPsychInitEditor extends React.Component {
         onTouchTap={this.handleClose}
       />,
     ];
+
+		const overlayStyle = {
+			position: 'absolute',
+			top: 0,
+			right: 0,
+			left: 0,
+			bottom: 0,
+			background: 'rgba(0,0,0,0.5)',
+			textAlign: 'center',
+			color: '#fff'
+		}
+
+		let mediaList = null;
+		if(typeof this.state.s3files.Contents !== 'undefined'){
+			mediaList = this.state.s3files.Contents.map( (f, i) =>
+				<ListItem
+					key={f.ETag}
+					primaryText={f.Key}
+					leftIcon={fileIconFromTitle(f.Key)}
+					rightIconButton={
+						<IconButton
+							onTouchTap={() => {this.handleSelect(i)}}
+							>
+							{this.state.selected[i] ? <CheckYesIcon /> : <CheckNoIcon />}
+						</IconButton>}
+				/>)
+		}
 
     return (
         <div className="mediaManager">
@@ -75,18 +192,16 @@ export default class jsPsychInitEditor extends React.Component {
             onRequestClose={this.handleClose}
             autoScrollBodyContent={true}
           >
-						<Dropzone onDrop={this.onDrop.bind(this)}></Dropzone>
-						<ul>
-	            {
-	              this.state.files.map(f => <li>{f.name} - {f.size} bytes</li>)
-	            }
-	          </ul>
-						<FlatButton
-			        label="Upload Files"
-			        primary={true}
-			        keyboardFocused={true}
-			        onTouchTap={this.handleUpload}
-			      />
+						<Dropzone
+							disableClick
+							onDrop={this.onDrop.bind(this)}
+							onDragEnter={this.handleEnter}
+							onDragLeave={this.handleExit}
+							style={{width:"100%", minHeight: '200px', position: 'relative'}}>
+							{this.state.uploadComplete && <List>{mediaList}</List>}
+							{!this.state.uploadComplete && <CircularProgress size={80} thickness={5} />}
+							{this.state.dropzoneActive && <div style={overlayStyle}>Drop files...</div>}
+						</Dropzone>
           </Dialog>
         </div>
     )
