ad733d24907b578a12f188fb19eac4a53ce4ad54
GavinQ1 <juqi@vassar.edu>
2018-01-17 17:47:38 -0500

    Restructure: css styling approach, prepare for mui-v1

--- src/common/components/TimelineNodeOrganizer/index.js
+++ src/common/components/TimelineNodeOrganizer/index.js
@@ -19,13 +19,47 @@ import OpenDrawer from 'material-ui/svg-icons/navigation/chevron-right';
 import SortableTreeMenu from '../../containers/TimelineNodeOrganizer/SortableTreeMenu';
 
 import GeneralTheme from '../theme.js';
-import './TimelineNodeOrganizer.css';
 
 export const TREE_MENU_INDENT = 20;
 
 export const WIDTH = 285;
 
 const colors = GeneralTheme.colors;
+
+const style = {
+	TimelineNodeOrganizer: (open) => ({
+		width: (open) ? `${WIDTH}px` : "0px",
+		flexBasis: 'auto',
+		'WebkitTransition': 'all 0.4s ease',
+		'MozTransition': 'all 0.4s ease',
+		transition: 'all 0.4s ease',
+		height: '100%',
+		display: 'flex',
+		overflow: 'hidden',
+		flexDirection: 'row',
+		backgroundColor: colors.background
+	}),
+	TimelineNodeOrganizerContainer: {
+		height: '100%',
+		width: '100%',
+		position: 'relative',
+		flexGrow: '1',
+	},
+	TimelineNodeOrganizerContent: {
+		height: '100%',
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'column-reverse'
+	},
+	TimelineNodeSheet: {
+		overflow: 'auto',
+		flexGrow: 1,
+		maxWidth: '100%',
+		paddingLeft: '0px',
+		width: '100%',
+		position: 'relative'
+	}
+}
 
 const avatarStyle = {
 	backgroundColor: colors.secondaryDeep
@@ -38,18 +72,11 @@ class TimelineNodeOrganizer extends React.Component {
 
 	render() {
 		return (
-		<div className="TimelineNode-Organizer"
-			 style={{
-			 	width: (this.props.open) ? `${WIDTH}px` : "0px",
-			 	flexBasis: 'auto',
-				'WebkitTransition': 'all 0.4s ease',
-				'MozTransition': 'all 0.4s ease',
-				transition: 'all 0.4s ease',
-			}}>
-				<div className="TimelineNode-Organizer-Container" >
+		<div className="TimelineNode-Organizer" style={style.TimelineNodeOrganizer(this.props.open)}>
+				<div className="TimelineNode-Organizer-Container" style={style.TimelineNodeOrganizerContainer}>
 					{(this.props.open) ?
-					<div className="TimelineNode-Organizer-Content">
-					<div className="TimelineNode-Sheet">
+					<div className="TimelineNode-Organizer-Content" style={style.TimelineNodeOrganizerContent}>
+					<div className="TimelineNode-Sheet" style={style.TimelineNodeSheet}>
 						<SortableTreeMenu
 							openTimelineEditorCallback={this.props.openTimelineEditorCallback}
 							closeTimelineEditorCallback={this.props.closeTimelineEditorCallback}
