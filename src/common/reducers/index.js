871b4609f55b6449f936d4b72ef6e5a4dc5c1608
Junyan Qi <Junyan.Qi@apps.ask.com>
2018-06-12 17:13:46 -0400

    Clear files

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,6 +1,4 @@
-// import * as actionTypes from '../constants/ActionTypes';
 import { combineReducers } from "redux";
-import reduceReducers from 'reduce-reducers';
 import experitmentReducer from './Experiment';
 import userReducer from './User';
 import notificationsReducer from './Notifications';
