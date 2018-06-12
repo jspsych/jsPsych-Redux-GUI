8ac2b9fc3d4a3148a87d3f9fcbad27815430b749
Junyan Qi <Junyan.Qi@apps.ask.com>
2018-06-12 17:08:28 -0400

    Remove unnecessary files

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from "redux";
 import reduceReducers from 'reduce-reducers';
 import experitmentReducer from './Experiment';
 import userReducer from './User';
-import backendReducer from './Backend';
 import notificationsReducer from './Notifications';
 import authenticationsReducer from './Authentications';
 
@@ -14,6 +13,4 @@ const combinedReducers = combineReducers({
 	authentications: authenticationsReducer
 });
 
-const rootReducer = reduceReducers(combinedReducers, backendReducer);
-
-export default rootReducer;
+export default combinedReducers;
