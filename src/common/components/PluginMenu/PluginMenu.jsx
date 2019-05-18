b7cfdd985192c44ab724ff3fd3e99157d10a7dc9
GavinQ1 <juqi@vassar.edu>
2019-05-18 15:03:49 -0400

    checkpoint: search textfield for plugin menu

--- src/common/components/PluginMenu/PluginMenu.jsx
+++ src/common/components/PluginMenu/PluginMenu.jsx
@@ -7,10 +7,16 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
 
 import Typography from '@material-ui/core/Typography';
 
 import ImageIcon from '@material-ui/icons/ImageOutlined';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const jsPsych = window.jsPsych;
 const PluginList = Object.keys(jsPsych.plugins || {}).filter((t) => (t !== 'parameterType' && t !== 'universalPluginParameters'));
@@ -30,6 +36,11 @@ const styles = theme => ({
     marginTop: '10px',
     marginBottom: '10px',
   },
+  searchFieldBackIcon: {
+    padding: 10,
+    paddingLeft: 0,
+    marginRight: 15,
+  },
   pluginItemWrapper: {
     width: "98%",
     margin: '0 auto',
@@ -38,16 +49,37 @@ const styles = theme => ({
   pluginList: {
     overflowY: 'auto',
     width: '100%'
-  }
+  },
+  searchFieldRoot: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    margin: '0 auto',
+    marginTop: '10px',
+  },
+  searchFieldContainer: {
+    width: "75%",
+  },
+  searchFieldFocused: {
+    color: "#388e3c",
+  },
+  searchFieldUnderline: {
+    '&:after': {
+      borderBottomColor: "#388e3c",
+    },
+  },
 })
 
 
 const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
+const toNormalName = str => str.split("-").map(capitalize).join(" ");
+
 const PluginItem = props => {
     const { value, isLast, classes } = props;
 
-    let displayed = value.split("-").map(capitalize).join(" ");
+    let displayed = toNormalName(value);
 
     return (
         <div className={classes.pluginItemWrapper}>
@@ -72,18 +104,70 @@ const PluginItem = props => {
     )
 }
 
+const isPrefix = (keyword, str) => {
+  keyword = keyword.toLowerCase().trim();
+  str = str.toLowerCase().trim();
+  if (keyword.length > str.length) return false;
+  for (let i = 0; i < keyword.length; i++) {
+    let c1 = keyword.charAt(i), c2 = str.charAt(i);
+    if (c1 !== c2) return false;
+  }
+  return true;
+}
+
+const isPrefixEitherWay = (keyword, txt) => {
+  return isPrefix(keyword, txt) || isPrefix(keyword, toNormalName(txt));
+}
 
 class PluginMenu extends React.Component {
+    state = {
+      keyword: '',
+    };
+
+    handleKeywordChange = event => {
+      this.setState({
+        keyword: event.target.value,
+      });
+    };
+
     render() {
       const { 
         classes,
       } = this.props;
 
+      const {
+        keyword,
+      } = this.state;
+
+      let filteredList = PluginList.filter(txt => isPrefixEitherWay(keyword, txt));
+
       return (
         <div className={classes.pluginMenu}>
+        <div className={classes.searchFieldRoot}>
+          <IconButton className={classes.searchFieldBackIcon} aria-label="Back">
+            <ArrowBackIcon />
+          </IconButton>
+          <div className={classes.searchFieldContainer}>
+            <Input
+                classes={{
+                  focused: classes.searchFieldFocused,
+                  underline: classes.searchFieldUnderline,
+                }}
+                id="input-with-icon-textfield"
+                fullWidth
+                endAdornment={
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                }
+                onChange={this.handleKeywordChange}
+            />
+          </div>
+        </div>
+        <Divider className={classes.divider}/>
          <List component="nav" className={classes.pluginList}>
           {
-            PluginList && PluginList.map((val, i) => {
+            filteredList.map((val, i) => {
                 return (
                     <PluginItem 
                         value={val} 
